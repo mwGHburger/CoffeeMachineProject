b4bb83651168d6ff533f010c64363ed37cdb3e98
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-28 16:52:08 +1100

    passed all tests for the 4th iteration - generate dictionary, will continue working on money calculation function

--- CoffeeMachine.Tests/DrinkDictionaryGeneratorTests.cs
+++ CoffeeMachine.Tests/DrinkDictionaryGeneratorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Moq;
 using Xunit;
@@ -27,6 +28,24 @@ namespace CoffeeMachine.Tests
             var actual = drinkDictionaryGenerator.Generate(orders);
 
             Assert.Equal(2, actual["coffee"]);
+        }
+        
+        [Fact]
+        public void ShouldThrowExceptionWhenOrderListIsEmpty()
+        {
+            // Mock<IOrder> mockOrder1 = new Mock<IOrder>();
+            // Mock<IOrder> mockOrder2 = new Mock<IOrder>();
+            // Mock<IOrder> mockOrder3 = new Mock<IOrder>();
+            var orders = new List<IOrder>();
+            
+            // mockOrder1.Setup(x => x.DrinkType).Returns(new DrinkType("coffee", 0.6));
+            // mockOrder2.Setup(x => x.DrinkType).Returns(new DrinkType("coffee", 0.6));
+            // mockOrder3.Setup(x => x.DrinkType).Returns(new DrinkType("tea", 0.5));
+
+            var drinkDictionaryGenerator = new DrinkDictionaryGenerator();
+
+            var ex = Assert.Throws<Exception>(() => drinkDictionaryGenerator.Generate(orders));
+            Assert.Equal("Currently No Orders!", ex.Message);
         }
     }
 }
